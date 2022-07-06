6c4cfaf895139cafa19511d52020c1cae2b70f9e
Caio Leto <caioleto07@gmail.com>
2022-07-06 15:18:08 -0300

    jogo finalizado

--- js/game.js
+++ js/game.js
@@ -1,4 +1,6 @@
 const grid = document.querySelector('.grid')
+const spanPlayer = document.querySelector('.player')
+const timer = document.querySelector('.timer')
 
 const characters = [
     'beth',
@@ -28,7 +30,8 @@ const checkEndGame = () => {
     const disabledCards = document.querySelectorAll('.disabled-card')
 
     if (disabledCards.length === 20) {
-        alert('Parabéns, você conseguiu!')
+        clearInterval(this.loop)
+        alert(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi: ${timer.innerHTML}`)
     }
 }
 
@@ -59,7 +62,7 @@ const checkCards = () => {
 
 const revealCard = ({ target }) => {
 
-    if (target.parentNode.className.includes('reveak-card')) {
+    if (target.parentNode.className.includes('reveal-card')) {
         return
     }
 
@@ -109,4 +112,16 @@ const loadGame = () => {
 }
 
 
-loadGame()
+const startTimer = () => {
+
+    this.loop = setInterval(() => {
+        const currentTime = +timer.innerHTML
+        timer.innerHTML = currentTime + 1
+    }, 1000)
+}
+
+window.onload = () => {
+    spanPlayer.innerHTML = localStorage.getItem('player')
+    startTimer()
+    loadGame()
+}
